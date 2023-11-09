a9e80f15cdd297d1b7949841eedc9cd545fbd421
pablo44 <pawelrz@wp.pl>
2023-11-09 11:41:08 +0100

    storage file commit

---
+++ storage.py
@@ -0,0 +1,24 @@
+
+class Node:
+    def __init__(self, data):
+        """ Store the data, and set next to None"""
+
+    def __str__(self):
+        """ Return a string representation of the data """
+
+
+class Storage:
+    def __init__(self):
+        """ Creates an empty Storage class. Sets head to None. """
+
+    def push(self, data):
+        """ Create a Node to hold the data, then put it at the head of the list. """
+
+    def pop(self):
+        """ Remove the head Node and return its data. """
+
+    def peek(self):
+        """ Return the data from the head Node, without removing it. """
+
+    def isempty(self):
+        """ Return True if the list is empty, otherwise False """
